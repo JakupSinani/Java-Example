9ad8d401db6d9a017becd916ae98edbe06bbb71c
Js28130-crypto <64150019+Js28130-crypto@users.noreply.github.com>
2022-08-28 03:45:39 +0200

    Example

---
+++ src/main/java/com/example/e/demo/AccountTestWithMainMethod.java
@@ -0,0 +1,22 @@
+package com.example.e.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AccountTestWithMainMethod {
+    public static void main(String[] args) {
+        SavingsAccount savingsAccount=new SavingsAccount(100,"Jakup",500);
+        SalaryAccount salaryAccount=new SalaryAccount(101,"Artim",500);
+        List<Account> accountList = new ArrayList<Account>();
+        accountList.add(savingsAccount);
+        accountList.add(salaryAccount);
+        salaryAccount.deposit(500);
+        salaryAccount.withdraw(750);
+
+        savingsAccount.deposit(500);
+        savingsAccount.withdraw(750);
+        for (Account ac:accountList){
+            System.out.println(ac);
+        }
+    }
+}
